b2ca9d99e09f308f6d9f0fc8fe2cdd686b9a2de0
Anderson Paim <andersonpaim15@outlook.com>
2022-09-29 20:56:45 -0300

    Added to the snake to chase orbs

--- AISnakeBot2022/Assets/Scripts/Behaviours/BotMussurana.cs
+++ AISnakeBot2022/Assets/Scripts/Behaviours/BotMussurana.cs
@@ -5,37 +5,121 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "AIBehaviours/BotMussuna")]
 public class BotMussurana : AIBehaviour
 {
+    private enum State
+    {
+        IDLE, EATING, RUNNING, WANDER
+    }
+
+    private Collider2D[] _collidersList;
+
+    private List<GameObject> _opponentsList = new List<GameObject>();
+    private List<GameObject> _orbsList = new List<GameObject>();
+    private State _currentState = State.WANDER;
+    private Transform _targetObject;
+
     public override void Init(GameObject own, SnakeMovement ownMove)
     {
         base.Init(own, ownMove);
         ownerMovement.StartCoroutine(UpdateDirEveryXSeconds(timeChangeDir));
-
-
-
     }
 
     //seria interessante ter um controlador com o colisor que define o mundo pra poder gerar pontos dentro desse colisor
     public override void Execute()
     {
-        MoveForward();
+        //MoveForward();
+        GetNearbyObjects();
+        BotController();
+        //COMENDO
+        //FUGINDO
+        //ANDANDO ALEATORIAMENTE
+        //ATACAR
     }
 
     //ia basica, move, muda de direcao e move
     private void MoveForward()
     {
-        MouseRotationSnake();
-        owner.transform.position = Vector2.MoveTowards(owner.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), ownerMovement.speed * Time.deltaTime);
+        //MouseRotationSnake();
+        //owner.transform.position = Vector2.MoveTowards(owner.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), ownerMovement.speed * Time.deltaTime);
     }
 
-    private void MouseRotationSnake()
+    private void GetNearbyObjects()
     {
+        _collidersList = Physics2D.OverlapCircleAll(owner.transform.position, 10);
 
-        direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - owner.transform.position;
+        _opponentsList.Clear();
+        _orbsList.Clear();
+
+        foreach (Collider2D collider in _collidersList)
+        {
+            if(collider.tag == "Body")
+            {
+                if(Vector3.Distance(collider.gameObject.transform.position, owner.transform.position) < 1f)
+                {
+                    _opponentsList.Add(collider.gameObject);
+                }
+            }
+            else if(collider.tag == "Orb")
+            {
+                _orbsList.Add(collider.gameObject);
+            }
+        }
+    }
+
+    private void BotController()
+    {
+        owner.transform.position = Vector2.MoveTowards(owner.transform.position, _targetObject.transform.position, ownerMovement.speed * Time.deltaTime);
+
+        if(_currentState == State.WANDER)
+        {
+            if(_orbsList.Count > 0)
+            {
+                GetOrb();
+            }
+        }
+        else if(_currentState == State.EATING)
+        {
+            VerifyTarget();
+        }
+    }
+
+    private void GetOrb()
+    {
+        GameObject nearestOrb = null;
+
+        foreach(GameObject orb in _orbsList)
+        {
+            if(nearestOrb == null)
+            {
+                nearestOrb = orb;
+            }
+            else
+            {
+                float lastDist = Vector3.Distance(nearestOrb.gameObject.transform.position, owner.gameObject.transform.position);
+                float newDist = Vector3.Distance(orb.gameObject.transform.position, owner.gameObject.transform.position);
+
+                if(newDist < lastDist)
+                {
+                    nearestOrb = orb;
+                }
+            }
+        }
+
+        direction = nearestOrb.gameObject.transform.position - owner.transform.position;
         direction.z = 0.0f;
+        _targetObject = nearestOrb.transform;
+        _currentState = State.EATING;
+    }
 
-        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
-        owner.transform.rotation = Quaternion.Slerp(owner.transform.rotation, rotation, ownerMovement.speed * Time.deltaTime);
+    private void VerifyTarget()
+    {
+        if(_targetObject == null)
+        {
+            _currentState = State.WANDER;
+        }
+        else
+        {
+            owner.transform.position = Vector2.MoveTowards(owner.transform.position, _targetObject.transform.position, ownerMovement.speed * Time.deltaTime);
+        }
     }
 
     private IEnumerator UpdateDirEveryXSeconds(float x)
@@ -58,10 +142,5 @@ public class BotMussurana : AIBehaviour
         direction.z = 0.0f;
 
         ownerMovement.StartCoroutine(UpdateDirEveryXSeconds(x));*/
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("INIMIGO");
     }
 }
